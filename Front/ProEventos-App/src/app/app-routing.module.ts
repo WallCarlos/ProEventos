99f8b3fd7b8990c57598b8e4ffd0d134e5114b7e
Wallace Carlos <wallace.carlos@brasilwm.com.br>
2022-08-08 12:48:28 -0300

    Angular - Organizando, Rotas, Alertas e Mais

---
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+
+const routes: Routes = [
+  { path: 'eventos', component: EventosComponent },
+  { path: 'palestrantes', component: PalestrantesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+
+import { EventoService } from '../../services/evento.service';
+import { Evento } from '../../models/Evento';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css'],
+  //providers: [EventoService]
+})
+export class EventosComponent implements OnInit {
+  modalRef?: BsModalRef;
+
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+
+  public larguraImagem = 150;
+  public margemImagem = 2;
+  public exibirImagem = true;
+
+  private filtroListado: string = '';
+
+  public get filtroLista(): string {
+    return this.filtroListado;
+  }
+
+  public set filtroLista(value: string) {
+    this.filtroListado = value;
+
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {}
+
+  public ngOnInit(): void {
+    this.getEventos();
+    /** spinner starts on init */
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+    }, 1000);
+  }
+
+  public alterarImagem(): void {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
+  public getEventos(): void {
+    this.eventoService.getEvento().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Erro ao carregar os Eventos.', 'Erro!');
+      },
+      complete: () => this.spinner.hide(),
+    });
+  }
+
+  public openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  public confirm(): void {
+    this.modalRef?.hide();
+    this.toastr.success('Evento excluído com sucesso.', 'Excluído!');
+  }
+
+  public decline(): void {
+    this.modalRef?.hide();
+  }
+}
